c638acaee88ad17ecba1286dd77a20b5042a0198
350611906a <beaconwwj@163.com>
2021-12-06 09:33:22 +0800

    修复shape错误的bug

--- source_code/chapter5_cnn/channels.py
+++ source_code/chapter5_cnn/channels.py
@@ -8,21 +8,26 @@ X = torch.tensor([[[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]],
                [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]])
 K = torch.tensor([[[0.0, 1.0], [2.0, 3.0]], [[1.0, 2.0], [3.0, 4.0]]])
 
+print("X.shape: ", X.shape, "K.shape: ", K.shape)
 conv_result = corr2d_multi_in(X, K)
 print(conv_result.shape)
 
 def corr2d_multi_in_out(X, K):
-    return torch.stack([corr2d_multi_in(X, K) for k in K], 0)
+    # 迭代“K”的第0个维度，每次都对输入“X”执行互相关运算。
+    # 最后将所有结果都叠加在一起
+    return torch.stack([corr2d_multi_in(X, k) for k in K], 0)
 
 # 堆叠方法
-# print(K.shape)
-# K = torch.stack((K, K + 1, K + 2), 0)
-# print(K.shape)
+print(K.shape)
+K = torch.stack((K, K + 1, K + 2), 0)
+print(K.shape)
 
+# 多通道卷积输入输出
 conv_result = corr2d_multi_in_out(X, K)
 print(conv_result.shape)
 
 # 1x1卷积
+# 验证一个1x1的卷积，等价于一个全连接
 def corr2d_multi_in_out_1x1(X, K):
     c_i, h, w = X.shape
     c_o = K.shape[0]
@@ -35,6 +40,11 @@ def corr2d_multi_in_out_1x1(X, K):
 X = torch.normal(0, 1, (3, 3, 3))
 K = torch.normal(0, 1, (2, 3, 1, 1))
 
+# 验证二者相等
+# 使用全连接替换卷积
 Y1 = corr2d_multi_in_out_1x1(X, K)
+# 正常的使用卷积
 Y2 = corr2d_multi_in_out(X, K)
+print(Y1)
+print(Y2)
 assert float(torch.abs(Y1 - Y2).sum()) < 1e-6
